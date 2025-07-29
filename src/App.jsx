c3284651c107d5f654bdf640a73cb0bb7d1d4228
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-07-29 21:52:58 +0545

    no_idea

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+function App() {
+  return (<>
+    <div className=" w-full min-h-screen bg-black text-white">
+      <h1 className="text-4xl">yyo</h1>
+
+    </div>
+    <div className="flex items-center justify-center h-screen">
+
+    </div>
+  </>
+  );
+}
+
+export default App;
