e943413f57add528d631a593bbda727af8cd6a17
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-07-30 21:40:58 +0545

    sa.es

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@ import "./index.css";
 function App() {
   return (<>
     <div className=" w-full min-h-screen bg-black text-white">
-      <h1 className="text-4xl">yyo</h1>
 
     </div>
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-screen bg-gradient-to-b from-black to-gray-400">
 
     </div>
   </>
