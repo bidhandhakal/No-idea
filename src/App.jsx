4a29edf336eefb06658c1bb9834c54fab0379375
BIDHAN DHAKAL <bidhandhakal.web@gmail.com>
2025-08-01 12:14:49 +0545

    nav2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./index.css";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
@@ -7,15 +7,24 @@ function App() {
       <nav className="flex justify-between items-center p-15 px-8 md:px-16 lg:px-24 bg-[#F0F1FA]">
         <div className="flex items-center gap-2">
           <a href="/" className="flex items-center gap-2">
-            <h1 className="text-3xl font-bold tracking-widest text-black">NOIDEA</h1>
+            <div
+              className="text-4xl tracking-wide font-medium text-black "
+              aria-label="NOIDEA"
+            >
+              NOIDEA
+            </div>
           </a>
         </div>
-        <div>
-          <Button className="bg-[#2B2E3A] text-white text-base font-sans font-bold hover:bg-[#3A47F4] cursor-pointer w-30 h-13 rounded-full transition-all duration-400 p-2 mr-4">
-            HIRE ME
+        <div className="flex items-center gap-4">
+          <Button className="bg-[#2B2E3A] text-white text-[1.2rem] font-semibold hover:bg-[#3A47F4] cursor-pointer w-35 h-13 rounded-full transition-all duration-400 p-2 mr-4">
+            HIRE ME &nbsp;<span class="header-right-menu-btn-dot"></span>
           </Button>
-          <Button className="bg-[#F0F1FA] text-black text-base font-sans font-bold hover:bg-[#ffffff] cursor-pointer w-30 h-13 rounded-full transition-all duration-400 p-2">
+          <Button className="menu-button bg-[#F0F1] text-black text-[1.2rem] font-semibold hover:bg-[#ffffff] cursor-pointer w-35 h-13 rounded-full transition-all duration-400 p-2">
             MENU
+            <div className="dots-container">
+              <span className="header-right-menu-btn-dot"></span>
+              <span className="header-right-menu-btn-dot"></span>
+            </div>
           </Button>
         </div>
       </nav>
